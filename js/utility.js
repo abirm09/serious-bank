b316fd8a564793145faccf8408285c575fe2a661
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-03-09 13:04:42 +0600

    Added some cool functions

--- js/utility.js
+++ js/utility.js
@@ -12,6 +12,12 @@ export const setInnerTextById = (id, value) => {
   element.innerHTML = value;
 };
 
+//get input value text
+export const getInputTextValueById = id => {
+  const input = document.getElementById(id);
+  const inputValue = input.value;
+  return inputValue;
+};
 //get localStorage data
 export const localStorageData = () => {
   const getData = JSON.parse(localStorage.getItem("transaction"));
@@ -27,4 +33,48 @@ export const setDataOnLocalStorage = data => {
 export const updateDataOnLocalStorage = data => {
   const previOusData = localStorageData();
   previOusData.push(data);
+  setDataOnLocalStorage(previOusData);
+  // console.log(localStorageData());
+};
+//random string
+export const randomString = () => {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < 6) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+};
+//get total withdraw or total deposit
+export const totalDepositOrWithdraw = command => {
+  const localData = localStorageData();
+  const filteredDta = [];
+  if (command === "deposit") {
+    localData.filter(x => {
+      if (x.trName === "deposit") {
+        return filteredDta.push(x.trAmount);
+      }
+    });
+  } else if (command === "withdraw") {
+    localData.filter(x => {
+      if (x.trName === "withdraw") {
+        return filteredDta.push(parseFloat(Math.abs(x.trAmount)));
+      }
+    });
+  }
+  const total = filteredDta.reduce((previous, current) => {
+    return previous + current;
+  }, 0);
+  return total;
+};
+export const totalAvailable = () => {
+  const localData = localStorageData();
+  let ab = 0;
+  const available = localData.map(x => {
+    ab += parseFloat(x.trAmount);
+  });
+  return ab;
 };
--- main.js
+++ main.js
@@ -10,7 +10,11 @@ changeTheme(selectedTheme);
 import {
   getInputValueById,
   setInnerTextById,
-  localStorageData,
+  randomString,
+  updateDataOnLocalStorage,
+  getInputTextValueById,
+  totalDepositOrWithdraw,
+  totalAvailable,
 } from "./js/utility.js";
 
 //set localStorage
@@ -22,12 +26,89 @@ if (localStorage.getItem("transaction") === null) {
 //get deposit amount
 document.getElementById("deposit-btn").addEventListener("click", () => {
   const inputAmount = getInputValueById("deposit-amount-input");
+  const remarks = getInputTextValueById("deposit-remarks");
   if (isNaN(inputAmount)) {
     alert("Please check your input,and try again.");
     return;
   } else if (inputAmount <= 0) {
     alert("Amount should greater than 1.");
     return;
+  } else if (remarks === "") {
+    alert("Have to field remarks.");
+    return;
   }
-  setInnerTextById("deposit-amount", inputAmount);
+  const dateObj = new Date();
+  const date = `${dateObj.getDate()}/${
+    dateObj.getMonth() + 1
+  }/${dateObj.getFullYear()}`;
+  const time = `${
+    dateObj.getHours() > 12 ? dateObj.getHours() - 12 : dateObj.getHours()
+  }:${dateObj.getMinutes()}:${dateObj.getSeconds()} ${
+    dateObj.getHours() > 12 ? "PM" : "AM"
+  }`;
+  const trId = `${dateObj.getMinutes()}${randomString()}${
+    dateObj.getSeconds() < 10
+      ? `9${dateObj.getSeconds()}`
+      : dateObj.getSeconds()
+  }`;
+
+  const newTransaction = {
+    trId: trId,
+    trDate: date,
+    trTime: time,
+    trName: "deposit",
+    remarks: remarks,
+    trAmount: inputAmount,
+  };
+  updateDataOnLocalStorage(newTransaction);
+ const totalDeposit= totalDepositOrWithdraw("deposit")
+ setInnerTextById("deposit-amount", totalDeposit);
 });
+
+//withdraw function
+document.getElementById("withdraw-btn").addEventListener("click", () => {
+  const inputAmount = getInputValueById("withdraw-amount-input");
+  const remarks = getInputTextValueById("withdraw-remarks");
+  if (isNaN(inputAmount)) {
+    alert("Please check your input,and try again.");
+    return;
+  } else if (inputAmount <= 0) {
+    alert("Amount should greater than 1.");
+    return;
+  } else if (remarks === "") {
+    alert("Have to field remarks.");
+    return;
+  }
+  const dateObj = new Date();
+  const date = `${dateObj.getDate()}/${
+    dateObj.getMonth() + 1
+  }/${dateObj.getFullYear()}`;
+  const time = `${
+    dateObj.getHours() > 12 ? dateObj.getHours() - 12 : dateObj.getHours()
+  }:${dateObj.getMinutes()}:${dateObj.getSeconds()} ${
+    dateObj.getHours() > 12 ? "PM" : "AM"
+  }`;
+  const trId = `${dateObj.getMinutes()}${randomString()}${
+    dateObj.getSeconds() < 10
+      ? `9${dateObj.getSeconds()}`
+      : dateObj.getSeconds()
+  }`;
+
+  const newTransaction = {
+    trId: trId,
+    trDate: date,
+    trTime: time,
+    trName: "withdraw",
+    remarks: remarks,
+    trAmount: `-${inputAmount}`,
+  };
+  updateDataOnLocalStorage(newTransaction);
+ const total= totalDepositOrWithdraw("withdraw")
+ setInnerTextById("withdraw-amount", total);
+});
+//display total translation 
+const globalTotalDeposit= totalDepositOrWithdraw("deposit")
+setInnerTextById("deposit-amount", globalTotalDeposit);
+const globalTotalWithdraw= totalDepositOrWithdraw("withdraw")
+setInnerTextById("withdraw-amount", globalTotalWithdraw);
+totalAvailable()
